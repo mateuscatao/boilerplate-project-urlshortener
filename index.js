ac563a8af230b4d5b768ce21e354822403badaea
mateuscatao <mateuscatao1311@gmail.com>
2023-11-28 15:17:30 -0300

    solution

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+let isUrl = require ('is-url');
+let bodyParser = require ('body-parser');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -14,9 +17,29 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// My solution
+
+let shortUrlCounter = 0;
+const urlsObject = {};
+
+app.use("/api/shorturl", bodyParser.urlencoded({ extend:false }));
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+
+  if (!isUrl(url)) return res.json({ error: "invalid url" });
+
+  shortUrlCounter += 1;
+  urlsObject[shortUrlCounter] = url;
+  
+  res.json({ original_url: url, short_url: shortUrlCounter });
+});
+
+app.get("/api/shorturl/:shortenedUrl", (req, res) => {
+  const shortenedUrl = req.params.shortenedUrl;
+  const url = urlsObject[shortenedUrl];
+
+  res.status(301).redirect(url);
 });
 
 app.listen(port, function() {
